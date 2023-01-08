f691d4930e2686ca9d1c38f4d00446a643ad47f4
Ryder Belserion <no-reply@ryderbelserion.com>
2023-01-08 14:13:59 -0500

    [ci-skip] Add optional dependencies to modrinth task

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -42,6 +42,10 @@ tasks {
 
         autoAddDependsOn.set(true)
 
+        dependencies {
+            optional.project("essentialsx")
+        }
+
         gameVersions.addAll(listOf("1.8", "1.9", "1.10", "1.11", "1.12", "1.13", "1.14", "1.15", "1.16", "1.17", "1.18", "1.19"))
         loaders.addAll(listOf("spigot", "paper", "purpur"))
 
