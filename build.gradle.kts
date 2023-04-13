59ca6216b1c06d3f8496a99004bf6f1613fdd000
Ryder Belserion <no-reply@ryderbelserion.com>
2023-04-12 23:32:35 -0400

    Fix undentified version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,11 @@ val color = if (beta) betaUpdate else releaseUpdate
 val repo = if (beta) "beta" else "releases"
 
 val url = "https://modrinth.com/$extension/${rootProject.name.lowercase()}/versions"
-val download = "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
 val msg = if (beta) "New version of ${rootProject.name} is ready!" else "New version of ${rootProject.name} is ready! <@&929463441159254066>"
 
 rootProject.version = "3.9.1.1"
+
+val download = "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
 
 webhook {
     this.avatar("https://en.gravatar.com/avatar/${WebhookExtension.Gravatar().md5Hex("no-reply@ryderbelserion.com")}.jpeg")
