81579b5551c7eb606328076f268d42e739628036
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-05 12:13:52 -0400

    No tab completion needed for staffchat

--- src/main/java/me/h1dd3nxn1nja/chatmanager/commands/CommandStaffChat.java
+++ src/main/java/me/h1dd3nxn1nja/chatmanager/commands/CommandStaffChat.java
@@ -8,12 +8,17 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import me.h1dd3nxn1nja.chatmanager.utils.BossBarUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class CommandStaffChat implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandStaffChat implements CommandExecutor, TabCompleter {
 
 	@NotNull
 	private final ChatManager plugin = ChatManager.get();
@@ -101,5 +106,10 @@ public class CommandStaffChat implements CommandExecutor {
 		}
 
 		return true;
+	}
+
+	@Override
+	public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+		return new ArrayList<>();
 	}
 }
