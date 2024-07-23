075651e9cf2049184ac8e85aebcc6c9741bca16d
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-23 12:20:22 -0400

    set supported version to 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ tasks {
             "version" to rootProject.version,
             "group" to rootProject.group,
             "description" to rootProject.description,
-            "apiVersion" to "1.20.6",
+            "apiVersion" to libs.versions.minecraft.get(),
             "authors" to providers.gradleProperty("authors").get(),
             "website" to providers.gradleProperty("website").get()
         )
@@ -109,7 +109,7 @@ tasks {
 
         syncBodyFrom.set(rootProject.file("README.md").readText(Charsets.UTF_8))
 
-        gameVersions.set(listOf("1.20.6", "1.21"))
+        gameVersions.set(listOf(libs.versions.minecraft.get()))
 
         loaders.addAll(listOf("paper", "purpur"))
 
