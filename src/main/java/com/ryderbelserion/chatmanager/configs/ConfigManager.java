21dbe3fbe04d31b7c92f2d5e70831214bcec26bb
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-21 18:22:22 -0400

    rules have been moved to rules.yml

--- src/main/java/com/ryderbelserion/chatmanager/configs/ConfigManager.java
+++ src/main/java/com/ryderbelserion/chatmanager/configs/ConfigManager.java
@@ -5,6 +5,8 @@ import ch.jalu.configme.SettingsManagerBuilder;
 import ch.jalu.configme.resource.YamlFileResourceOptions;
 import com.ryderbelserion.chatmanager.ChatManager;
 import com.ryderbelserion.chatmanager.configs.types.SpamKeys;
+import com.ryderbelserion.chatmanager.configs.types.rules.RuleKeys;
+import com.ryderbelserion.chatmanager.configs.types.spy.ModKeys;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 
@@ -14,14 +16,47 @@ public class ConfigManager {
 
     private static final ChatManager plugin = JavaPlugin.getPlugin(ChatManager.class);
 
+    private static SettingsManager config;
+    private static SettingsManager rules;
     private static SettingsManager spam;
 
     public static void load() {
+        config = SettingsManagerBuilder
+                .withYamlFile(new File(plugin.getDataFolder(), "config.yml"), builder)
+                .useDefaultMigrationService()
+                .configurationData(ModKeys.class)
+                .create();
+
+        rules = SettingsManagerBuilder
+                .withYamlFile(new File(plugin.getDataFolder(), "rules.yml"), builder)
+                .useDefaultMigrationService()
+                .configurationData(RuleKeys.class)
+                .create();
+
         spam = SettingsManagerBuilder
                 .withYamlFile(new File(plugin.getDataFolder(), "spam.yml"), builder)
                 .useDefaultMigrationService()
                 .configurationData(SpamKeys.class)
                 .create();
+    }
+
+    /**
+     * Refreshes configuration files.
+     */
+    public static void refresh() {
+        config.reload();
+
+        rules.reload();
+
+        spam.reload();
+    }
+
+    public static SettingsManager getConfig() {
+        return config;
+    }
+
+    public static SettingsManager getRules() {
+        return rules;
     }
 
     public static SettingsManager getSpam() {
