7bdf94be4e9929dd43e207ca696bcc64aa518674
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-21 17:03:02 -0400

    register more listeners

--- src/main/java/com/ryderbelserion/chatmanager/ChatManager.java
+++ src/main/java/com/ryderbelserion/chatmanager/ChatManager.java
@@ -4,6 +4,8 @@ import com.ryderbelserion.chatmanager.api.CustomMetrics;
 import com.ryderbelserion.chatmanager.cache.CacheListener;
 import com.ryderbelserion.chatmanager.cache.UserManager;
 import com.ryderbelserion.chatmanager.configs.ConfigManager;
+import com.ryderbelserion.chatmanager.listeners.chat.ChatListener;
+import com.ryderbelserion.chatmanager.listeners.chat.DelayListener;
 import com.ryderbelserion.vital.paper.Vital;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -26,6 +28,9 @@ public class ChatManager extends Vital {
 
         // Register cache listener!
         getServer().getPluginManager().registerEvents(new CacheListener(), this);
+
+        getServer().getPluginManager().registerEvents(new DelayListener(), this);
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
 
         new CustomMetrics().start();
     }
