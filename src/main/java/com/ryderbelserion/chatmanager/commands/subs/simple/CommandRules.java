bb44aecbab04c2aa0fd9fca441b5005438712f6b
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-21 18:30:58 -0400

    add rules commands

--- src/main/java/com/ryderbelserion/chatmanager/commands/subs/simple/CommandRules.java
+++ src/main/java/com/ryderbelserion/chatmanager/commands/subs/simple/CommandRules.java
@@ -1,24 +1,36 @@
 package com.ryderbelserion.chatmanager.commands.subs.simple;
 
+import ch.jalu.configme.SettingsManager;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.ryderbelserion.chatmanager.ChatManager;
+import com.ryderbelserion.chatmanager.configs.ConfigManager;
+import com.ryderbelserion.chatmanager.configs.types.rules.RuleKeys;
 import com.ryderbelserion.vital.paper.api.commands.Command;
 import com.ryderbelserion.vital.paper.api.commands.CommandData;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import me.h1dd3nxn1nja.chatmanager.Methods;
 import org.bukkit.Server;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 import org.jetbrains.annotations.NotNull;
+import static io.papermc.paper.command.brigadier.Commands.argument;
 
 public class CommandRules extends Command {
 
     private final ChatManager plugin = ChatManager.get();
     private final Server server = this.plugin.getServer();
 
+    private final SettingsManager rules = ConfigManager.getRules();
+
     @Override
     public void execute(final CommandData data) {
+        final int page = data.getIntegerArgument("page");
 
+        this.rules.getProperty(RuleKeys.rules).getRules().get(page).forEach(line -> Methods.sendMessage(data.getCommandSender(), line, true));
     }
 
     @Override
@@ -28,13 +40,19 @@ public class CommandRules extends Command {
 
     @Override
     public @NotNull final LiteralCommandNode<CommandSourceStack> literal() {
-        return Commands.literal("rules")
-                .requires(source -> source.getSender().hasPermission(getPermission()))
-                .executes(context -> {
-                    execute(new CommandData(context));
+        final LiteralArgumentBuilder<CommandSourceStack> root = Commands.literal("rules").requires(source -> source.getSender().hasPermission(getPermission()));
 
-                    return com.mojang.brigadier.Command.SINGLE_SUCCESS;
-                }).build();
+        final RequiredArgumentBuilder<CommandSourceStack, Integer> arg1 = argument("min", IntegerArgumentType.integer()).suggests((ctx, builder) -> {
+            this.rules.getProperty(RuleKeys.rules).getRules().keySet().forEach(builder::suggest);
+
+            return builder.buildFuture();
+        }).executes(context -> {
+            execute(new CommandData(context));
+
+            return com.mojang.brigadier.Command.SINGLE_SUCCESS;
+        });
+
+        return root.then(arg1).build();
     }
 
     @Override
