2a99905ca47a46185e5e539b79081addeabb7de3
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-22 18:53:33 -0400

    update permission check

--- src/main/java/com/ryderbelserion/chatmanager/listeners/staff/SpyListener.java
+++ src/main/java/com/ryderbelserion/chatmanager/listeners/staff/SpyListener.java
@@ -2,6 +2,7 @@ package com.ryderbelserion.chatmanager.listeners.staff;
 
 import ch.jalu.configme.SettingsManager;
 import com.ryderbelserion.chatmanager.ChatManager;
+import com.ryderbelserion.chatmanager.api.enums.Files;
 import com.ryderbelserion.chatmanager.api.enums.chat.SpyState;
 import com.ryderbelserion.chatmanager.api.enums.other.Messages;
 import com.ryderbelserion.chatmanager.api.enums.other.Permissions;
@@ -10,11 +11,13 @@ import com.ryderbelserion.chatmanager.api.cache.UserManager;
 import com.ryderbelserion.chatmanager.api.cache.objects.User;
 import com.ryderbelserion.chatmanager.configs.ConfigManager;
 import com.ryderbelserion.chatmanager.configs.types.ConfigKeys;
+import com.ryderbelserion.chatmanager.utils.LogUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import java.util.Calendar;
 import java.util.HashMap;
 
 public class SpyListener implements Listener {
@@ -56,14 +59,16 @@ public class SpyListener implements Listener {
     public void onSocialSpy(MessageSendEvent event) {
         if (!this.config.getProperty(ConfigKeys.toggle_social_spy)) return;
 
-        final Player player = event.getPlayer();
-
-        if (player.hasPermission(Permissions.BYPASS_COMMAND_SPY.getNode())) return;
-
         final String message = event.getMessage();
 
         for (final String command : this.config.getProperty(ConfigKeys.command_spy_commands)) {
             if (message.toLowerCase().startsWith(command)) return;
+        }
+
+        final Player player = event.getPlayer();
+
+        if (player != null) { // check if player is null
+            if (player.hasPermission(Permissions.BYPASS_COMMAND_SPY.getNode())) return; // if player has bypass, we don't send to staff.
         }
 
         final Player target = event.getTarget();
