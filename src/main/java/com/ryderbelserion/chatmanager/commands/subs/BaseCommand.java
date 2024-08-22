4d5ec567178136f41deac2b59ee6ddc07d18269f
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-22 16:41:09 -0400

    add protected config getter

--- src/main/java/com/ryderbelserion/chatmanager/commands/subs/BaseCommand.java
+++ src/main/java/com/ryderbelserion/chatmanager/commands/subs/BaseCommand.java
@@ -1,6 +1,8 @@
 package com.ryderbelserion.chatmanager.commands.subs;
 
+import ch.jalu.configme.SettingsManager;
 import com.ryderbelserion.chatmanager.ChatManager;
+import com.ryderbelserion.chatmanager.configs.ConfigManager;
 import dev.triumphteam.cmd.core.annotations.Command;
 import org.bukkit.Server;
 
@@ -9,5 +11,7 @@ public abstract class BaseCommand {
 
     protected final ChatManager plugin = ChatManager.get();
     protected final Server server = this.plugin.getServer();
+
+    protected final SettingsManager config = ConfigManager.getConfig();
 
 }
