024de7ec79a93eef1b63c4168fb4e7556a46989a
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-22 00:10:54 -0400

    update config handling

--- src/main/java/com/ryderbelserion/chatmanager/configs/ConfigManager.java
+++ src/main/java/com/ryderbelserion/chatmanager/configs/ConfigManager.java
@@ -4,11 +4,20 @@ import ch.jalu.configme.SettingsManager;
 import ch.jalu.configme.SettingsManagerBuilder;
 import ch.jalu.configme.resource.YamlFileResourceOptions;
 import com.ryderbelserion.chatmanager.ChatManager;
+import com.ryderbelserion.chatmanager.configs.impl.messages.messages.ErrorKeys;
+import com.ryderbelserion.chatmanager.configs.impl.messages.messages.MiscKeys;
+import com.ryderbelserion.chatmanager.configs.impl.messages.messages.PlayerKeys;
+import com.ryderbelserion.chatmanager.configs.types.MessageKeys;
 import com.ryderbelserion.chatmanager.configs.types.SpamKeys;
-import com.ryderbelserion.chatmanager.configs.types.rules.RuleKeys;
 import com.ryderbelserion.chatmanager.configs.types.ConfigKeys;
+import com.ryderbelserion.vital.common.managers.files.CustomFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.simpleyaml.configuration.ConfigurationSection;
+import org.simpleyaml.configuration.file.YamlFile;
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ConfigManager {
 
@@ -16,28 +25,25 @@ public class ConfigManager {
 
     private static final ChatManager plugin = JavaPlugin.getPlugin(ChatManager.class);
 
+    private static final Map<Integer, List<String>> rules = new HashMap<>();
+
     private static SettingsManager config;
-    private static SettingsManager rules;
-    private static SettingsManager spam;
+    private static SettingsManager messages;
 
     public static void load() {
         config = SettingsManagerBuilder
                 .withYamlFile(new File(plugin.getDataFolder(), "config.yml"), builder)
                 .useDefaultMigrationService()
-                .configurationData(ConfigKeys.class)
+                .configurationData(ConfigKeys.class, SpamKeys.class)
                 .create();
 
-        rules = SettingsManagerBuilder
-                .withYamlFile(new File(plugin.getDataFolder(), "rules.yml"), builder)
+        messages = SettingsManagerBuilder
+                .withYamlFile(new File(plugin.getDataFolder(), "messages.yml"), builder)
                 .useDefaultMigrationService()
-                .configurationData(RuleKeys.class)
+                .configurationData(PlayerKeys.class, ErrorKeys.class, MiscKeys.class, MessageKeys.class)
                 .create();
 
-        spam = SettingsManagerBuilder
-                .withYamlFile(new File(plugin.getDataFolder(), "spam.yml"), builder)
-                .useDefaultMigrationService()
-                .configurationData(SpamKeys.class)
-                .create();
+        populateRules();
     }
 
     /**
@@ -46,20 +52,40 @@ public class ConfigManager {
     public static void refresh() {
         config.reload();
 
-        rules.reload();
-
-        spam.reload();
+        populateRules();
     }
 
     public static SettingsManager getConfig() {
         return config;
     }
 
-    public static SettingsManager getRules() {
-        return rules;
+    public static SettingsManager getMessages() {
+        return messages;
     }
 
-    public static SettingsManager getSpam() {
-        return spam;
+    public static void populateRules() {
+        rules.clear();
+
+        final CustomFile customFile = plugin.getFileManager().getFile("rules.yml");
+
+        if (customFile != null) {
+            YamlFile file = customFile.getConfiguration();
+
+            if (file != null) {
+                ConfigurationSection section = file.getConfigurationSection("rules");
+
+                if (section != null) {
+                    section.getKeys(false).forEach(key -> {
+                        final List<String> rule = section.getStringList(key);
+
+                        rules.put(Integer.parseInt(key), rule);
+                    });
+                }
+            }
+        }
+    }
+
+    public static Map<Integer, List<String>> getRules() {
+        return rules;
     }
 }
