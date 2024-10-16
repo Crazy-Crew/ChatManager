ec21338d72a06d1631bcfd0b3ca0abc7c9ef4a85
Ryder Belserion <no-reply@ryderbelserion.com>
2024-10-15 22:31:46 -0400

    how did this compile on my pc?

--- paper/src/main/java/me/h1dd3nxn1nja/chatmanager/commands/CommandLists.java
+++ paper/src/main/java/me/h1dd3nxn1nja/chatmanager/commands/CommandLists.java
@@ -13,6 +13,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class CommandLists implements CommandExecutor {
 
 	private final ChatManager plugin = JavaPlugin.getPlugin(ChatManager.class);
