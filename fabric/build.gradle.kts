d93dd0a15c42a2bf3d4ddab06229f0e14c9426b4
Ryder Belserion <no-reply@ryderbelserion.com>
2024-10-22 12:33:08 -0400

    add fabric module

---
+++ fabric/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.shadow)
+}
+
+dependencies {
+    implementation(project(":common"))
+}
+
+tasks {
+    assemble {
+        dependsOn(shadowJar)
+
+        doLast {
+            copy {
+                from(shadowJar.get())
+                into(rootProject.projectDir.resolve("jars"))
+            }
+        }
+    }
+
+    shadowJar {
+        archiveBaseName.set(rootProject.name)
+        archiveClassifier.set("")
+    }
+}
