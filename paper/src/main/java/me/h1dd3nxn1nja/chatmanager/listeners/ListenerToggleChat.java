9a67fceaa7fd28a9f4c457897b4024a893d6833f
Ryder Belserion <contact@ryderbelserion.com>
2025-04-27 16:28:03 -0400

    Update toggle chat listener to use AsyncChatEvent

--- paper/src/main/java/me/h1dd3nxn1nja/chatmanager/listeners/ListenerToggleChat.java
+++ paper/src/main/java/me/h1dd3nxn1nja/chatmanager/listeners/ListenerToggleChat.java
@@ -1,26 +1,40 @@
 package me.h1dd3nxn1nja.chatmanager.listeners;
 
+import com.ryderbelserion.chatmanager.ApiLoader;
+import com.ryderbelserion.chatmanager.api.cmds.ToggleChatData;
+import io.papermc.paper.event.player.AsyncChatEvent;
 import me.h1dd3nxn1nja.chatmanager.ChatManager;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.identity.Identity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.jetbrains.annotations.NotNull;
+import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 public class ListenerToggleChat implements Listener {
 
-	@NotNull
 	private final ChatManager plugin = ChatManager.get();
 
+	private final ApiLoader api = this.plugin.api();
+
+	private final ToggleChatData data = this.api.getToggleChatData();
+
 	@EventHandler(ignoreCancelled = true)
-	public void onChat(AsyncPlayerChatEvent event) {
-		Player player = event.getPlayer();
+	public void onChatEvent(AsyncChatEvent event) {
+		final Player player = event.getPlayer();
 
-		if (this.plugin.api().getToggleChatData().containsUser(player.getUniqueId())) return;
+		final UUID uuid = player.getUniqueId();
 
-		Set<Player> recipients = event.getRecipients();
+		if (this.data.containsUser(uuid)) return;
 
-		recipients.removeIf(cm -> this.plugin.api().getToggleChatData().containsUser(cm.getUniqueId()));
+		final Set<Audience> viewers = event.viewers();
+
+		viewers.removeIf(viewer -> {
+			final Optional<UUID> optional = viewer.get(Identity.UUID);
+
+            return optional.filter(this.data::containsUser).isPresent();
+        });
 	}
 }
