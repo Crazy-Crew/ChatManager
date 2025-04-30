4104b03febcee8c9b532a825cbab84e86728f3a8
Ryder Belserion <contact@ryderbelserion.com>
2025-04-30 00:27:58 -0400

    Add more protected variables

--- paper/src/main/java/com/ryderbelserion/chatmanager/commands/AnnotationFeature.java
+++ paper/src/main/java/com/ryderbelserion/chatmanager/commands/AnnotationFeature.java
@@ -1,5 +1,7 @@
 package com.ryderbelserion.chatmanager.commands;
 
+import com.ryderbelserion.chatmanager.managers.ServerManager;
+import com.ryderbelserion.chatmanager.managers.UserManager;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import me.h1dd3nxn1nja.chatmanager.ChatManager;
 import org.incendo.cloud.annotations.AnnotationParser;
@@ -8,6 +10,10 @@ import org.jetbrains.annotations.NotNull;
 public abstract class AnnotationFeature {
 
     protected final ChatManager plugin = ChatManager.get();
+
+    protected final UserManager userManager = this.plugin.getUserManager();
+
+    protected final ServerManager serverManager = this.plugin.getServerManager();
 
     public abstract void registerFeature(@NotNull final AnnotationParser<CommandSourceStack> parser);
 
