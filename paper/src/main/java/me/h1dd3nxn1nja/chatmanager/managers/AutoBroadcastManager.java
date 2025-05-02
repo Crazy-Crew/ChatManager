503f59be38e631e5d72a234b18ca87087b85e2a4
Ryder Belserion <contact@ryderbelserion.com>
2025-05-02 01:28:19 -0400

    Update broadcasts

--- paper/src/main/java/me/h1dd3nxn1nja/chatmanager/managers/AutoBroadcastManager.java
+++ paper/src/main/java/me/h1dd3nxn1nja/chatmanager/managers/AutoBroadcastManager.java
@@ -1,6 +1,7 @@
 package me.h1dd3nxn1nja.chatmanager.managers;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import com.ryderbelserion.chatmanager.enums.Files;
 import com.ryderbelserion.fusion.paper.api.enums.Scheduler;
@@ -8,6 +9,7 @@ import com.ryderbelserion.fusion.paper.api.scheduler.FoliaScheduler;
 import me.h1dd3nxn1nja.chatmanager.ChatManager;
 import me.h1dd3nxn1nja.chatmanager.Methods;
 import org.bukkit.Server;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import me.h1dd3nxn1nja.chatmanager.utils.BossBarUtil;
@@ -22,11 +24,13 @@ public class AutoBroadcastManager {
 	private static final List<World> worlds = new ArrayList<>();
 	
 	public static void globalMessages() {
-		FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
+		final FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
 
-		String prefix = autobroadcast.getString("Auto_Broadcast.Global_Messages.Prefix", "&7[&6AutoBroadcast&7] &r");
-		int interval = autobroadcast.getInt("Auto_Broadcast.Global_Messages.Interval", 30);
-		List<String> messages = autobroadcast.getStringList("Auto_Broadcast.Global_Messages.Messages");
+		final boolean header = autobroadcast.getBoolean("Auto_Broadcast.Global_Messages.Header_And_Footer", false);
+
+		final String prefix = autobroadcast.getString("Auto_Broadcast.Global_Messages.Prefix", "&7[&6AutoBroadcast&7] &r");
+		final int interval = autobroadcast.getInt("Auto_Broadcast.Global_Messages.Interval", 30);
+		final List<String> messages = autobroadcast.getStringList("Auto_Broadcast.Global_Messages.Messages");
 
 		new FoliaScheduler(Scheduler.global_scheduler) {
 			int line = 0;
@@ -34,17 +38,21 @@ public class AutoBroadcastManager {
 			@Override
 			public void run() {
 				if (autobroadcast.getBoolean("Auto_Broadcast.Global_Messages.Enable", false)) {
-					for (Player player : server.getOnlinePlayers()) {
-						if (autobroadcast.getBoolean("Auto_Broadcast.Global_Messages.Header_And_Footer", false)) {
-							Methods.sendMessage(player, prefix, autobroadcast.getString("Auto_Broadcast.Global_Messages.Header"), false);
-							Methods.sendMessage(player, prefix, messages.get(line).replace("{prefix}", prefix).replace("\\n", "\n"), false);
-							Methods.sendMessage(player, prefix, autobroadcast.getString("Auto_Broadcast.Global_Messages.Footer"), false);
-						} else {
-							Methods.sendMessage(player, prefix, messages.get(line).replace("{prefix}", prefix).replace("\\n", "\n"), false);
-						}
+					line = 0;
 
-						Methods.playSound(player, autobroadcast, "Auto_Broadcast.Global_Messages.sound");
+					cancel();
+				}
+
+				for (final Player player : server.getOnlinePlayers()) {
+					if (header) {
+						Methods.sendMessage(player, prefix, autobroadcast.getString("Auto_Broadcast.Global_Messages.Header"), false);
+						Methods.sendMessage(player, prefix, messages.get(line).replace("{prefix}", prefix).replace("\\n", "\n"), false);
+						Methods.sendMessage(player, prefix, autobroadcast.getString("Auto_Broadcast.Global_Messages.Footer"), false);
+					} else {
+						Methods.sendMessage(player, prefix, messages.get(line).replace("{prefix}", prefix).replace("\\n", "\n"), false);
 					}
+
+					Methods.playSound(player, autobroadcast, "Auto_Broadcast.Global_Messages.sound");
 				}
 
 				line++;
@@ -55,43 +63,60 @@ public class AutoBroadcastManager {
 	}
 	
 	public static void perWorldMessages() {
-		FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
+		final FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
 
-		String prefix = autobroadcast.getString("Auto_Broadcast.Per_World_Messages.Prefix", "&7[&6AutoBroadcast&7] &r");
-		int interval = autobroadcast.getInt("Auto_Broadcast.Per_World_Messages.Interval", 60);
+		final String prefix = autobroadcast.getString("Auto_Broadcast.Per_World_Messages.Prefix", "&7[&6AutoBroadcast&7] &r");
+		final int interval = autobroadcast.getInt("Auto_Broadcast.Per_World_Messages.Interval", 60);
 
 		worlds.clear();
 
-		for (String key : autobroadcast.getConfigurationSection("Auto_Broadcast.Per_World_Messages.Messages").getKeys(false)) {
-			World w = new World(key, autobroadcast.getStringList("Auto_Broadcast.Per_World_Messages.Messages." + key), 0);
+		final ConfigurationSection section = autobroadcast.getConfigurationSection("Auto_Broadcast.Per_World_Messages.Messages");
 
-			worlds.add(w);
+		if (section != null) {
+			for (final String key : section.getKeys(false)) {
+				worlds.add(new World(key, autobroadcast.getStringList("Auto_Broadcast.Per_World_Messages.Messages." + key), 0));
+			}
 		}
+
+		final boolean header = autobroadcast.getBoolean("Auto_Broadcast.Per_World_Messages.Header_And_Footer", false);
 
 		new FoliaScheduler(Scheduler.global_scheduler) {
 
 			@Override
 			public void run() {
-				for (World world : getWorld()) {
-					if (autobroadcast.getBoolean("Auto_Broadcast.Per_World_Messages.Enable", false)) {
-						for (Player player : server.getOnlinePlayers()) {
-							if (player.getWorld().getName().equals(world.getName())) {
-								if (autobroadcast.getBoolean("Auto_Broadcast.Per_World_Messages.Header_And_Footer", false)) {
-									Methods.sendMessage(player, prefix, autobroadcast.getString("Auto_Broadcast.Per_World_Messages.Header", "&7*&7&m--------------------------------&7*"), false);
-									Methods.sendMessage(player, prefix, world.getMessages().get(world.getIndex()), false);
-									Methods.sendMessage(player, prefix, autobroadcast.getString("Auto_Broadcast.Per_World_Messages.Footer", "&7*&7&m--------------------------------&7*"), false);
-								} else {
-									Methods.sendMessage(player, prefix, world.getMessages().get(world.getIndex()).replaceAll("\\n", "\n"), false);
-								}
+				if (autobroadcast.getBoolean("Auto_Broadcast.Per_World_Messages.Enable", false)) {
+					worlds.clear();
 
-								Methods.playSound(player, autobroadcast, "Auto_Broadcast.Per_World_Messages.sound");
-							}
+					cancel();
+				}
+
+				final Collection<? extends Player> players = server.getOnlinePlayers();
+
+				for (final World world : getWorld()) {
+					final List<String> messages = world.getMessages();
+					final int index = world.getIndex();
+
+					final String message = messages.get(index);
+
+					final String name = world.getName();
+
+					for (final Player player : players) {
+						final String worldName = player.getWorld().getName();
+
+						if (!worldName.equals(name)) continue;
+
+						if (header) {
+							Methods.sendMessage(player, prefix, autobroadcast.getString("Auto_Broadcast.Per_World_Messages.Header", "&7*&7&m--------------------------------&7*"), false);
+							Methods.sendMessage(player, prefix, message, false);
+							Methods.sendMessage(player, prefix, autobroadcast.getString("Auto_Broadcast.Per_World_Messages.Footer", "&7*&7&m--------------------------------&7*"), false);
+						} else {
+							Methods.sendMessage(player, prefix, message.replaceAll("\\n", "\n"), false);
 						}
+
+						Methods.playSound(player, autobroadcast, "Auto_Broadcast.Per_World_Messages.sound");
 					}
 
-					int index = world.getIndex();
-
-					if (index + 1 < world.getMessages().size())
+					if (index + 1 < messages.size())
 						world.setIndex(index + 1);
 					else {
 						world.setIndex(0);
@@ -100,13 +125,13 @@ public class AutoBroadcastManager {
 			}
 		}.runAtFixedRate(0L, 20L * interval);
 	}
-	
-	public static void actionbarMessages() {
-		FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
 
-		String prefix = autobroadcast.getString("Auto_Broadcast.Actionbar_Messages.Prefix", "&7[&6AutoBroadcast&7] &r");
-		int interval = autobroadcast.getInt("Auto_Broadcast.Actionbar_Messages.Interval", 60);
-		List<String> messages = autobroadcast.getStringList("Auto_Broadcast.Actionbar_Messages.Messages");
+	public static void actionbarMessages() {
+		final FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
+
+		final String prefix = autobroadcast.getString("Auto_Broadcast.Actionbar_Messages.Prefix", "&7[&6AutoBroadcast&7] &r");
+		final int interval = autobroadcast.getInt("Auto_Broadcast.Actionbar_Messages.Interval", 60);
+		final List<String> messages = autobroadcast.getStringList("Auto_Broadcast.Actionbar_Messages.Messages");
 
 		new FoliaScheduler(Scheduler.global_scheduler) {
 			int line = 0;
@@ -114,11 +139,15 @@ public class AutoBroadcastManager {
 			@Override
 			public void run() {
 				if (autobroadcast.getBoolean("Auto_Broadcast.Actionbar_Messages.Enable", false)) {
-					for (Player player : server.getOnlinePlayers()) {
-						player.sendActionBar(Methods.placeholders(true, player, messages.get(line).replace("{prefix}", prefix).replace("{Prefix}", prefix)));
+					line = 0;
 
-						Methods.playSound(player, autobroadcast, "Auto_Broadcast.Actionbar_Messages.sound");
-					}
+					cancel();
+				}
+
+				for (final Player player : server.getOnlinePlayers()) {
+					player.sendActionBar(Methods.placeholders(true, player, messages.get(line).replace("{prefix}", prefix).replace("{Prefix}", prefix)));
+
+					Methods.playSound(player, autobroadcast, "Auto_Broadcast.Actionbar_Messages.sound");
 				}
 
 				line++;
@@ -129,10 +158,10 @@ public class AutoBroadcastManager {
 	}
 	
 	public static void titleMessages() {
-		FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
+		final FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
 
-		int interval = autobroadcast.getInt("Auto_Broadcast.Title_Messages.Interval", 60);
-		List<String> messages = autobroadcast.getStringList("Auto_Broadcast.Title_Messages.Messages");
+		final int interval = autobroadcast.getInt("Auto_Broadcast.Title_Messages.Interval", 60);
+		final List<String> messages = autobroadcast.getStringList("Auto_Broadcast.Title_Messages.Messages");
 
 		new FoliaScheduler(Scheduler.global_scheduler) {
 			int line = 0;
@@ -140,13 +169,17 @@ public class AutoBroadcastManager {
 			@Override
 			public void run() {
 				if (autobroadcast.getBoolean("Auto_Broadcast.Title_Messages.Enable", false)) {
-					for (Player player : server.getOnlinePlayers()) {
-						String title = Methods.placeholders(true, player, autobroadcast.getString("Auto_Broadcast.Title_Messages.Title"));
+					line = 0;
 
-						player.sendTitle(title, Methods.placeholders(true, player, messages.get(line)), 40, 20, 40);
+					cancel();
+				}
 
-						Methods.playSound(player, autobroadcast, "Auto_Broadcast.Title_Messages.sound");
-					}
+				for (final Player player : server.getOnlinePlayers()) {
+					final String title = Methods.placeholders(true, player, autobroadcast.getString("Auto_Broadcast.Title_Messages.Title"));
+
+					player.sendTitle(title, Methods.placeholders(true, player, messages.get(line)), 40, 20, 40);
+
+					Methods.playSound(player, autobroadcast, "Auto_Broadcast.Title_Messages.sound");
 				}
 
 				line++;
@@ -157,30 +190,34 @@ public class AutoBroadcastManager {
 	}
 	
 	public static void bossBarMessages() {
-		FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
+		final FileConfiguration autobroadcast = Files.AUTO_BROADCAST.getConfiguration();
 
-		int interval = autobroadcast.getInt("Auto_Broadcast.Bossbar_Messages.Interval", 60);
-		int time = autobroadcast.getInt("Auto_Broadcast.Bossbar_Messages.Bar_Time", 10);
-		List<String> messages = autobroadcast.getStringList("Auto_Broadcast.Bossbar_Messages.Messages");
+		final int interval = autobroadcast.getInt("Auto_Broadcast.Bossbar_Messages.Interval", 60);
+		final int time = autobroadcast.getInt("Auto_Broadcast.Bossbar_Messages.Bar_Time", 10);
+		final List<String> messages = autobroadcast.getStringList("Auto_Broadcast.Bossbar_Messages.Messages");
 
 		new FoliaScheduler(Scheduler.global_scheduler) {
 			int line = 0;
 
 			@Override
 			public void run() {
-				if (autobroadcast.getBoolean("Auto_Broadcast.Bossbar_Messages.Enable", false)) {
-					for (Player player : server.getOnlinePlayers()) {
-						BossBarUtil bossBar = new BossBarUtil(Methods.placeholders(true, player, messages.get(line)), org.bukkit.boss.BarColor.PINK, org.bukkit.boss.BarStyle.SOLID);
+				if (!autobroadcast.getBoolean("Auto_Broadcast.Bossbar_Messages.Enable", false)) {
+					line = 0;
 
-						if (autobroadcast.getInt("Auto_Broadcast.Bossbar_Messages.Bar_Time") == -1) {
-							bossBar.removeBossBar(player);
-							bossBar.setBossBar(player);
-						} else if (autobroadcast.getInt("Auto_Broadcast.Bossbar_Messages.Bar_Time") >= 0) {
-							bossBar.setBossBarTime(player, time);
-						}
+					cancel();
+				}
 
-						Methods.playSound(player, autobroadcast, "Auto_Broadcast.Bossbar_Messages.sound");
+				for (final Player player : server.getOnlinePlayers()) {
+					final BossBarUtil bossBar = new BossBarUtil(Methods.placeholders(true, player, messages.get(line)), org.bukkit.boss.BarColor.PINK, org.bukkit.boss.BarStyle.SOLID);
+
+					if (autobroadcast.getInt("Auto_Broadcast.Bossbar_Messages.Bar_Time") == -1) {
+						bossBar.removeBossBar(player);
+						bossBar.setBossBar(player);
+					} else if (autobroadcast.getInt("Auto_Broadcast.Bossbar_Messages.Bar_Time") >= 0) {
+						bossBar.setBossBarTime(player, time);
 					}
+
+					Methods.playSound(player, autobroadcast, "Auto_Broadcast.Bossbar_Messages.sound");
 				}
 
 				line++;
