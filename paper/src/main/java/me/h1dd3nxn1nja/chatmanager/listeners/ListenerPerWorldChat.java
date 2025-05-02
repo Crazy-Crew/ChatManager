708a97abd7276c6f43f70f8c54492dd607323545
Ryder Belserion <contact@ryderbelserion.com>
2025-05-01 20:40:49 -0400

    Update per world chat listeners

--- paper/src/main/java/me/h1dd3nxn1nja/chatmanager/listeners/ListenerPerWorldChat.java
+++ paper/src/main/java/me/h1dd3nxn1nja/chatmanager/listeners/ListenerPerWorldChat.java
@@ -3,14 +3,20 @@ package me.h1dd3nxn1nja.chatmanager.listeners;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import com.ryderbelserion.chatmanager.ApiLoader;
+import com.ryderbelserion.chatmanager.api.chat.PerWorldChatData;
+import com.ryderbelserion.chatmanager.api.chat.StaffChatData;
 import com.ryderbelserion.chatmanager.enums.Files;
+import io.papermc.paper.event.player.AsyncChatEvent;
 import me.h1dd3nxn1nja.chatmanager.ChatManager;
+import net.kyori.adventure.audience.Audience;
 import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class ListenerPerWorldChat implements Listener {
@@ -20,44 +26,79 @@ public class ListenerPerWorldChat implements Listener {
 
 	private final Server server = this.plugin.getServer();
 
-	@EventHandler
-	public void onWorldChat(AsyncPlayerChatEvent event) {
-		Player player = event.getPlayer();
-		String world = player.getWorld().getName();
-		UUID worldUID = event.getPlayer().getWorld().getUID();
-		Set<Player> recipients = event.getRecipients();
+	private final ApiLoader api = this.plugin.api();
+
+	private final StaffChatData staffData = this.api.getStaffChatData();
+
+	private final PerWorldChatData worldData = this.api.getPerWorldChatData();
+
+	@EventHandler(ignoreCancelled = true)
+	public void onWorldChat(AsyncChatEvent event) {
+		final Player player = event.getPlayer();
+		final UUID uuid = player.getUniqueId();
+		final World world = player.getWorld();
+		final UUID id = world.getUID();
+		final Set<Audience> recipients = event.viewers();
+
+		final FileConfiguration config = Files.CONFIG.getConfiguration();
+
+		if (!config.getBoolean("Per_World_Chat.Enable", false) || this.staffData.containsUser(uuid) || this.worldData.containsUser(uuid)) return;
+		if (config.getBoolean("Per_World_Chat.Group_Worlds.Enable", false)) return;
+
+		recipients.removeIf(target -> {
+			if (!(target instanceof Player instance)) {
+				return false;
+			}
+
+			final UUID player_uuid = instance.getUniqueId();
+			final UUID world_uuid = instance.getWorld().getUID();
+
+			return !this.worldData.containsUser(player_uuid) && id.equals(world_uuid);
+		});
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onPerWorldChat(AsyncChatEvent event) {
+		final Player player = event.getPlayer();
+		final World world = player.getWorld();
+		final String worldName = world.getName();
+		final Set<Audience> recipients = event.viewers();
+
+		final FileConfiguration config = Files.CONFIG.getConfiguration();
+
+		if (!config.getBoolean("Per_World_Chat.Group_Worlds.Enable", false)) return;
+
+		final ConfigurationSection section = config.getConfigurationSection("Per_World_Chat.Group_Worlds.Worlds");
 
 		List<String> playerGroup = null;
 
-		FileConfiguration config = Files.CONFIG.getConfiguration();
+		if (section != null) {
+			for (final String key : section.getKeys(false)) {
+				final List<String> group = config.getStringList("Per_World_Chat.Group_Worlds.Worlds." + key);
 
-		if (!config.getBoolean("Per_World_Chat.Enable", false)
-				|| this.plugin.api().getStaffChatData().containsUser(player.getUniqueId())) return;
+				if (group.contains(worldName)) {
+					playerGroup = group;
 
-		if (this.plugin.api().getPerWorldChatData().containsUser(player.getUniqueId())) return;
-
-		if (config.getBoolean("Per_World_Chat.Group_Worlds.Enable", false)) {
-			for (String key : config.getConfigurationSection("Per_World_Chat.Group_Worlds.Worlds").getKeys(false)) {
-				List<String> group = config.getStringList("Per_World_Chat.Group_Worlds.Worlds." + key);
-				if (group.contains(world)) playerGroup = group;
-			}
-
-			for (Player player2 : this.server.getOnlinePlayers()) {
-				String world2 = player2.getWorld().getName();
-				if (playerGroup != null) {
-					if (!playerGroup.contains(world2)) {
-						if (!this.plugin.api().getPerWorldChatData().containsUser(player2.getUniqueId())) recipients.remove(player2);
-					}
-				} else {
-					if (!world.equals(world2)) {
-						if (!this.plugin.api().getPerWorldChatData().containsUser(player2.getUniqueId())) recipients.remove(player2);
-					}
+					break;
 				}
 			}
-
-			return;
 		}
 
-		recipients.removeIf(players -> !worldUID.equals(players.getWorld().getUID()) && !this.plugin.api().getPerWorldChatData().containsUser(players.getUniqueId()));
+		for (final Player target : this.server.getOnlinePlayers()) {
+			final UUID targetUUID = target.getUniqueId();
+			final String targetWorld = target.getWorld().getName();
+
+			if (this.worldData.containsUser(targetUUID)) continue;
+
+			if (playerGroup != null && !playerGroup.contains(targetWorld)) {
+				recipients.remove(target);
+
+				return;
+			}
+
+			if (!worldName.equals(targetWorld)) {
+				recipients.remove(target);
+			}
+		}
 	}
 }
