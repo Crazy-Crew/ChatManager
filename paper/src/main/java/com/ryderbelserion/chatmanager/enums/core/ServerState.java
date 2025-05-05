7ba1683ef7bbcb63af1d248f2094e9801a804b6f
Ryder Belserion <contact@ryderbelserion.com>
2025-05-04 23:21:03 -0400

    Add name/comments to server state enum

--- paper/src/main/java/com/ryderbelserion/chatmanager/enums/core/ServerState.java
+++ paper/src/main/java/com/ryderbelserion/chatmanager/enums/core/ServerState.java
@@ -2,6 +2,22 @@ package com.ryderbelserion.chatmanager.enums.core;
 
 public enum ServerState {
 
-    MUTED
+    MUTED("muted", "Marks the server as muted which means no players can speak, unless they have bypass permissions.");
+
+    private final String description;
+    private final String name;
+
+    ServerState(final String name, final String description) {
+        this.description = description;
+        this.name = name;
+    }
+
+    public final String getDescription() {
+        return this.description;
+    }
+
+    public final String getName() {
+        return this.name;
+    }
 
 }
