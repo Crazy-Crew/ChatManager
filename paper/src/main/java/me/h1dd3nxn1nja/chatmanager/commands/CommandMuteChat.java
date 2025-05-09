4b0d262ba14c53a169eae39895c6edf2defcf08f
Ryder Belserion <contact@ryderbelserion.com>
2025-05-09 17:57:20 -0400

    Update mutechat command

--- paper/src/main/java/me/h1dd3nxn1nja/chatmanager/commands/CommandMuteChat.java
+++ paper/src/main/java/me/h1dd3nxn1nja/chatmanager/commands/CommandMuteChat.java
@@ -1,6 +1,9 @@
 package me.h1dd3nxn1nja.chatmanager.commands;
 
+import com.ryderbelserion.chatmanager.api.objects.PaperServer;
 import com.ryderbelserion.chatmanager.enums.Messages;
+import com.ryderbelserion.chatmanager.enums.core.ServerState;
+import com.ryderbelserion.chatmanager.managers.ServerManager;
 import me.h1dd3nxn1nja.chatmanager.ChatManager;
 import com.ryderbelserion.chatmanager.enums.Permissions;
 import me.h1dd3nxn1nja.chatmanager.Methods;
@@ -15,6 +18,8 @@ public class CommandMuteChat implements CommandExecutor {
 	@NotNull
 	private final ChatManager plugin = ChatManager.get();
 
+	private final ServerManager serverManager = this.plugin.getServerManager();
+
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
 		if (!(sender instanceof Player player)) {
@@ -24,14 +29,17 @@ public class CommandMuteChat implements CommandExecutor {
 		}
 
 		if (cmd.getName().equalsIgnoreCase("mutechat")) {
+			final PaperServer server = this.serverManager.getServer();
+
 			if (player.hasPermission(Permissions.COMMAND_MUTECHAT.getNode())) {
 				if (args.length == 0) {
-					if (Methods.isMuted()) {
-						Methods.setMuted();
+
+					if (server.hasState(ServerState.MUTED)) {
+						server.removeState(ServerState.MUTED);
 
 						Messages.MUTE_CHAT_BROADCAST_MESSAGES_ENABLED.sendMessage(player, "{player}", player.getName());
 					} else {
-						Methods.setMuted();
+						server.addState(ServerState.MUTED);
 
 						Messages.MUTE_CHAT_BROADCAST_MESSAGES_DISABLED.sendMessage(player, "{player}", player.getName());
 					}
@@ -47,8 +55,8 @@ public class CommandMuteChat implements CommandExecutor {
 			if (args[0].equalsIgnoreCase("-s")) {
 				if (player.hasPermission(Permissions.COMMAND_MUTECHAT_SILENT.getNode())) {
 					if (args.length == 1) {
-						if (Methods.isMuted()) {
-							Methods.setMuted();
+						if (server.hasState(ServerState.MUTED)) {
+							server.removeState(ServerState.MUTED);
 
 							for (Player staff : this.plugin.getServer().getOnlinePlayers()) {
 								if (staff.hasPermission(Permissions.BYPASS_MUTE_CHAT.getNode())) {
@@ -57,9 +65,8 @@ public class CommandMuteChat implements CommandExecutor {
 									return true;
 								}
 							}
-
 						} else {
-							Methods.setMuted();
+							server.addState(ServerState.MUTED);
 
 							for (Player staff : this.plugin.getServer().getOnlinePlayers()) {
 								if (staff.hasPermission(Permissions.BYPASS_MUTE_CHAT.getNode())) {
