b4bff965270144d9c1a82005ca91e97f679f2d9d
Ryder Belserion <contact@ryderbelserion.com>
2025-07-24 14:50:55 -0400

    [ci-skip] Change output directory

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -35,6 +35,13 @@ tasks {
     }
 
     shadowJar {
+        archiveBaseName.set("${rootProject.name}-${rootProject.version}")
+
+        copy {
+            from(project.layout.buildDirectory.dir("libs"))
+            into(rootProject.layout.buildDirectory.dir("libs"))
+        }
+
         listOf(
             "com.ryderbelserion.fusion",
             "org.bstats"
