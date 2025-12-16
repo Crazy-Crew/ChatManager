2f4dd1a93b89430899ca2c93e9a6c18df4f025c2
Ryder Belserion <contact@ryderbelserion.com>
2025-12-16 13:47:14 -0500

    Update jar output directories

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -43,26 +43,6 @@ tasks {
         }
     }
 
-    processResources {
-        filteringCharset = Charsets.UTF_8.name()
-
-        inputs.properties(
-            "name" to rootProject.name,
-            "version" to rootProject.version,
-            "description" to rootProject.description.toString(),
-            "minecraft" to libs.versions.minecraft.get(),
-            "group" to project.group
-        )
-
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE
-
-        with(copySpec {
-            from("src/main/resources/plugin.yml") {
-                expand(inputs.properties)
-            }
-        })
-    }
-
     runPaper.folia.registerTask()
 
     runServer {
